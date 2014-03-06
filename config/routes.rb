b267a9259b11ff1eaa6c9730802f10e257a0dcb3
Slava <slavapas13@gmail.com>
2014-03-06 12:50:44 +0200

    Added Deafult Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ArvutHouse::Application.routes.draw do
+  get "home/default"
+  root 'home#default'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
