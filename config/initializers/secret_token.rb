178fae7154b091b7d1937bfe343b82b414261525
Slava <slavapas13@gmail.com>
2014-03-06 12:37:34 +0200

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArvutHouse::Application.config.secret_key_base = '953b63650f75436cd1338cfe8a15c0576c51e0491c1b4ee6ef265ab057fc9926e3825ccc33d531ed33f503da8cda17115204e0fc9bc99dd61a4bc03743dcb27b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArvutHouse::Application.config.session_store :cookie_store, key: '_arvut_house_session'
