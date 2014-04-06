7df18195744de90f87fa4336d1931f5fa040bd4c
Slava <slavapas13@gmail.com>
2014-04-06 21:45:33 +0300

    Change index creation for evants table as separet change

--- db/migrate/20140404075359_create_events.rb
+++ db/migrate/20140404075359_create_events.rb
@@ -3,9 +3,8 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :name, limit: 60
       t.string :description
-      t.index("name"), unique: true
 
       t.timestamps
-    end
+    end    
   end
 end
