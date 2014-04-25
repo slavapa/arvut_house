db991f1ddd558c3451256cbd1ff0da0a124a3bd5
Slava <slavapas13@gmail.com>
2014-04-25 16:15:09 +0300

    Design Index pages and refactor translation

--- app/controllers/event_types_controller.rb
+++ app/controllers/event_types_controller.rb
@@ -5,7 +5,7 @@ class EventTypesController < ApplicationController
   # GET /event_types
   # GET /event_types.json
   def index
-    @event_types = EventType.all
+    @event_types = EventType.paginate(page: params[:page])
   end
 
   # GET /event_types/1
