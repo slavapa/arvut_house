b08c00008c101cbbbd213c98d2695714dbdafb3b
Slava <slavapas13@gamil.com>
2014-04-30 18:04:32 +0300

    Design Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,16 +1,18 @@
 class Person < ActiveRecord::Base
+  before_save :normalize_blank_values
   has_many :person_event_relationships, dependent: :destroy
   has_many :events, through: :person_event_relationships 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 60 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+            uniqueness: { case_sensitive: false }, if: lambda { |m| m.password.present? }
 
-  before_save { email.downcase! }
+  before_save { email.downcase! if  email.present?}
   before_create :create_remember_token
    
-  has_secure_password
+  has_secure_password validations: false
   validates :password, length: { minimum: 6 }, if: lambda { |m| m.password.present? }
+  validates_confirmation_of :password, if: lambda { |m| m.password.present? }
     
   def gender_arr
     @gender_arr = [['', nil], ['Male', '1'], ['Female', '2']]    
@@ -26,7 +28,12 @@ class Person < ActiveRecord::Base
   
     
 private
-
+# http://stackoverflow.com/questions/7202319/rails-force-empty-string-to-null-in-the-database
+  def normalize_blank_values
+    attributes.each do |column, value|
+      self[column].present? || self[column] = nil
+    end
+  end
   def create_remember_token
     self.remember_token = Person.hash(Person.new_remember_token)
   end
