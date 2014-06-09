9c2e0fdce7e5a16166de5098628367d1654fd3bc
slavapa <slavapas13@gmail.com>
2014-06-09 18:40:40 +0300

    Ada Sort functionality

--- app/models/person_search.rb
+++ app/models/person_search.rb
@@ -2,11 +2,12 @@
 class PersonSearch
   include SearchObject.module(:model, :sorting, :will_paginate)
   
-  scope { Person.all.order("name, family_name ASC") }
+  #scope { Person.all.order("name, family_name ASC") }
+  scope { Person.all }
   
   per_page 30
   
-  sort_by :name, :family_name, :gender, :status
+  sort_by :name, :family_name, :gender, :status_id
   
   option :person_id  
   option :gender 
@@ -20,6 +21,14 @@ class PersonSearch
     scope.where 'family_name LIKE ?', escape_search_term(value)
   end
   
+    
+  def sort_params_arr
+    [[I18n.t(:name), 'name asc'], 
+      [I18n.t('activerecord.attributes.person.family_name'), 'family_name asc'],
+      [I18n.t('activerecord.attributes.person.gender'), 'gender asc'], 
+      [I18n.t('activerecord.attributes.person.status_id'), 'status_id asc']]     
+  end 
+    
   
   private
   
