72deb9b7aff86512e6833a27ccec65fbb96a10dd
Slava <slavapas13@gmail.com>
2014-09-09 14:24:08 +0300

    Remove person details from updates query as it should not be shared in github

--- db/person_languages_fr_es.sql
+++
@@ -1,6 +0,0 @@
-
-insert into person_languages(person_id,language_id,created_at,updated_at) select id,4,NOW(),NOW()  from people where name ='מרק' and  family_name = 'ויגנה';
-insert into person_languages(person_id,language_id,created_at,updated_at) select id,4,NOW(),NOW()  from people where name ='גיל' and  family_name = 'אפינגר';
-insert into person_languages(person_id,language_id,created_at,updated_at) select id,5,NOW(),NOW()  from people where name ='ורדה' and  family_name = 'סלמן';
-insert into person_languages(person_id,language_id,created_at,updated_at) select id,5,NOW(),NOW()  from people where name ='גדעון' and  family_name = 'כהן';
-insert into person_languages(person_id,language_id,created_at,updated_at) select id,5,NOW(),NOW()  from people where name ='מרינה' and  family_name = 'נוחגר';
