b89a079d6cb6d3fa09d4583579b692e44526100f
Slava <slavapas13@gmail.com>
2015-04-17 07:31:49 +0300

    Populate poeple

---
+++ db/person_languages_eng.sql
@@ -0,0 +1,14 @@
+
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='נטע' and  family_name = 'קורן';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='מלי יהלי' and  family_name = 'אוחנה';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='שי' and  family_name = 'לבנת';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='בתיה' and  family_name = 'פז';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='שלומית' and  family_name = 'יצחקי';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='אורטל' and  family_name = 'גרנסקי';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='אסף' and  family_name = 'מצמון';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='שבתאי' and  family_name = 'נחום';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='תמיר' and  family_name = 'קהילה';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='ורדה' and  family_name = 'סלמן';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='מרינה' and  family_name = 'נוחגר';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='גיל' and  family_name = 'אפינגר';
+insert into person_languages(person_id,language_id,created_at,updated_at) select id,1,NOW(),NOW()  from people where name ='סרגיי' and  family_name = 'בסקין';
