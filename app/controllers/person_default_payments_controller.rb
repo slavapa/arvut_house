1e1b3b07352711022f6127a3e87c2ae444bdbd45
Slava Pasechnik <slavapas13@gmail.com>
2017-08-24 14:41:58 +0000

    Payment Type - default person payments

--- app/controllers/person_default_payments_controller.rb
+++ app/controllers/person_default_payments_controller.rb
@@ -6,28 +6,27 @@ class PersonDefaultPaymentsController < ApplicationController
     @payment_type = PaymentType.find(params[:person_default_payment][:payment_type_id])
     @person = Person.find(params[:person_default_payment][:person_id])
     @person_default_payment = @payment_type.add_person!(@person)
-    # #Person.merge_payment_attr(@person,  @person_payment)
-    # @person.merge_payment_attr(@person_payment)
     
-    # respond_to do |format|
-    #   format.html {redirect_to edit_payment_path(@payment)}
-    #   format.js      
-    # end
+    respond_to do |format|
+      format.html {redirect_to edit_payment_path(@payment)}
+      format.js      
+    end
   end
 
   def destroy
     @payment_type = PaymentType.find(params[:person_default_payment][:payment_type_id])
     @person = Person.find(params[:person_default_payment][:person_id])
     @payment_type.remove_person!(@person)
-    # respond_to do |format|
-    #   format.html {redirect_to edit_payment_path(@payment)}
-    #   format.js      
-    # end
+    
+    respond_to do |format|
+      format.html {redirect_to edit_payment_path(@payment)}
+      format.js      
+    end
   end
 
   def update
     @payment_type = PaymentType.find(@person_payment.payment_type_id)
-    # @person = Person.find(@person_payment.person_id)
+    @person = Person.find(@person_payment.person_id)
     
     if @person_payment.update_attributes(amount: params[:amount])
       flash.now[:success] = t(:person_payment_updated) 
@@ -35,8 +34,6 @@ class PersonDefaultPaymentsController < ApplicationController
     else
       flash.now[:error] = t(:person_payment_not_updated) 
     end 
-    
-    # @person.merge_payment_attr(@person_payment) 
     
     respond_to do |format|
       format.html {redirect_to edit_payment_type_path(@payment_type)}
