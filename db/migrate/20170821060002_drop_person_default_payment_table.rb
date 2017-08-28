c6ee17b604ece91e6912c02a5c5cd29637dcc282
Slava Pasechnik <slavapas13@gmail.com>
2017-08-28 17:44:26 +0000

    Commented out drop_table person_default_payments migration

--- db/migrate/20170821060002_drop_person_default_payment_table.rb
+++ db/migrate/20170821060002_drop_person_default_payment_table.rb
@@ -1,9 +1,9 @@
 class DropPersonDefaultPaymentTable < ActiveRecord::Migration
   def change
-    drop_table(:person_default_payments, if_exists: true)
+    # drop_table(:person_default_payments, if_exists: true)
      
-    if index_exists?(:person_default_payments, ["person_id", "payment_type_id"])
-      remove_index :person_default_payments, ["person_id", "payment_type_id"]
-    end
+    # if index_exists?(:person_default_payments, ["person_id", "payment_type_id"])
+    #   remove_index :person_default_payments, ["person_id", "payment_type_id"]
+    # end
   end
 end
